3aef90c3d36c6b3437a87e897e8b065774f8035e
Jonathan Mesfin <jonathanmesfin@Jonathans-MacBook-Pro.local>
2026-09-11 21:56:38 -0400

    Worked on the game class and created the gobliz and zombie subclasses

--- Game.java
+++ Game.java
@@ -1,9 +1,34 @@
 package terminalTrials;
+import java.util.Scanner;
+import java.util.Random;
 public class Game {
     //~ Fields ................................................................
+    private int totalWaves;
+    private int currentWave;
+    Scanner scan;
+    ArrayList<Enemy> enemies;
+    Player player;
 
     //~ Constructors ..........................................................
+    public Game(){
+        scan = new Scanner(System.in);
+        totalWaves = 0l
+        currentWave = 1;
+
+
+
+
+
+
+    }
 
     //~Public  Methods ........................................................
+    public void createWave(){
+        this.totalWaves += 1;
+        this.currentWave += 1
+        int randomNumber = rand.nextInt(2) +1;
+
+        
+    }
 
 }
---
+++ Goblin.java
@@ -0,0 +1,24 @@
+public class Goblin extends Enemy{
+
+Final int baseHealth;
+Final int baseDamage;
+
+public Goblin(double difficulty) {
+    this.baseHealt = 50;
+    this.baseDamage = 30;
+    
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
